85afa4f2de91eed5c420ecb44f6ff06524e58137
alex <test@MacBook-Air-test.local>
2024-09-08 16:40:32 +0300

    connecting to database

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"github.com/s21platform/society-service/internal/config"
+	Db "github.com/s21platform/society-service/internal/repository/postgres"
+	"log"
+	"os"
 )
 
 func main() {
 	// чтение конфига
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+
+	dbRepo, err := Db.New(cfg)
+
+	if err != nil {
+		log.Printf("db.New: %v", err)
+		os.Exit(1)
+	}
+
+	defer dbRepo.Close()
 }
---
+++ internal/repository/postgres/repository.go
@@ -0,0 +1,54 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/s21platform/society-service/internal/config"
+	"log"
+	"time"
+)
+
+type Repository struct {
+	connection *sql.DB
+}
+
+func connect(cfg *config.Config) (*Repository, error) {
+	// Connect db
+	conStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
+		cfg.Postgres.User, cfg.Postgres.Password, cfg.Postgres.Database, cfg.Postgres.Host, cfg.Postgres.Port)
+
+	db, err := sql.Open("postgres", conStr)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %w", err)
+	}
+
+	// Сhecking connection db
+	if err = db.Ping(); err != nil {
+		return nil, fmt.Errorf("db.Ping: %w", err)
+	}
+
+	return &Repository{db}, nil
+}
+
+func New(cfg *config.Config) (*Repository, error) {
+	var err error
+
+	var repo *Repository
+
+	for i := 0; i < 5; i++ {
+		repo, err = connect(cfg)
+		if err == nil {
+			return repo, nil
+		}
+
+		log.Println(err)
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	return nil, err
+}
+
+func (r *Repository) Close() {
+	r.connection.Close()
+}
