687d0e0ec991b86d324b98fd0e3bf2ccf05cffad
asepopov <asepopov@avito.ru>
2024-10-15 19:13:35 +0300

    fix migration

--- migrations/entrypoint_down_one.sh
+++ migrations/entrypoint_down_one.sh
@@ -3,4 +3,4 @@
 # Применим миграции
 # ВАЖНО! Для локального запуска вызвать из корня проекта, предварительно подгрузив переменные окружения в терминал командой  `set -a; source <путь к .env файлу>; set +a`
 # Установка goose: go install github.com/pressly/goose/v3/cmd/goose@latest
-goose -dir ./migrations postgres "user=$SOCIETY_SERVICE_POSTGRES_USER password=$SOCIETY_SERVICE_POSTGRES_PASSWORD dbname=$SOCIETY_SERVICE_POSTGRES_DB host=$SOCIETY_SERVICE_POSTGRES_HOST port=$FSOCIETY_SERVICE_POSTGRES_PORT sslmode=disable" down
+goose -dir ./migrations postgres "user=$SOCIETY_SERVICE_POSTGRES_USER password=$SOCIETY_SERVICE_POSTGRES_PASSWORD dbname=$SOCIETY_SERVICE_POSTGRES_DB host=$SOCIETY_SERVICE_POSTGRES_HOST port=$SOCIETY_SERVICE_POSTGRES_PORT sslmode=disable" down
