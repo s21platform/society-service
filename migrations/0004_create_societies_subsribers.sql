48bbe51fff0d14ef0fe4091cdae1a5e62d23a688
alex <test@MacBook-Air-test.local>
2024-11-19 19:02:38 +0300

    bugFixSocieties

---
+++ migrations/0004_create_societies_subsribers.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS societies_subscribers
+(
+    id BIGINT PRIMARY KEY,
+    society_id BIGINT NOT NULL,
+    user_uuid UUID NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT fk_society FOREIGN KEY (society_id) REFERENCES societies (id)
+    );
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS societies_subscribers;
+-- +goose StatementEnd
