bc37d6cada38bffa5876088c3061f0df4a91ce02
alex <test@MacBook-Air-test.local>
2024-11-30 22:00:28 +0300

    createGetSocietyWithOffset

--- internal/repository/postgres/repository.go
+++ internal/repository/postgres/repository.go
@@ -89,8 +89,9 @@ func (r *Repository) GetPermissions() (*[]model.GetPermissions, error) {
 
 func (r *Repository) GetSocietyWithOffset(socData *model.WithOffsetData) (*[]model.SocietyWithOffsetData, error) {
 	var data []model.SocietyWithOffsetData
-	query := "select name, photo_url avatar_link, s.id society_id, ss.user_uuid = $1 is_member" +
-		"  FROM societies s JOIN societies_subscribers ss ON s.id = ss.society_id " +
+	query := "SELECT name, photo_url avatar_link, s.id society_id, CASE" +
+		" WHEN ss.user_uuid = $1 THEN true ELSE false END AS is_member " +
+		"FROM societies s LEFT JOIN societies_subscribers ss ON s.id = ss.society_id AND ss.user_uuid = $1 " +
 		"WHERE name ILIKE $2 OFFSET $3 LIMIT $4"
 	err := r.connection.Select(&data, query, socData.Uuid, "%"+socData.Name+"%", socData.Offset, socData.Limit)
 	if err != nil {
