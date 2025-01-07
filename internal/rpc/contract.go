2612bd863c5683373de4f8e65e17adac96e24c43
alex <test@MacBook-Air-test.local>
2025-01-07 17:17:29 +0300

    added GetSocietiesForUser

--- internal/rpc/contract.go
+++ internal/rpc/contract.go
@@ -8,9 +8,9 @@ type DbRepo interface {
 	CreateGroup(socData *model.SocietyData) (int, error)
 	GetAccessLevel() (*[]model.AccessLevel, error)
 	GetPermissions() (*[]model.GetPermissions, error)
-	GetSocietyWithOffset(data *model.WithOffsetData) (*[]model.SocietyWithOffsetData, error)
+	GetSocietyWithOffset(data *model.WithOffsetData) (*[]model.SocietyWithOffsetData, int, error)
 	GetSocietyInfo(id int64) (*model.SocietyInfo, error)
 	SubscribeToSociety(id int64, uuid string) (bool, error)
 	UnsubscribeFromSociety(id int64, uuid string) (bool, error)
-	GetSocietiesForUser(uuid string, uuidUser string) (*[]model.SocietyWithOffsetData, int, error)
+	GetSocietiesForUser(uuid string, uuidUser string) (*[]model.SocietyWithOffsetData, error)
 }
