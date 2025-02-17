d7b3d9b587ed5efbf1849c7123c3f3fbea3ea252
alex <test@MacBook-Pro.local>
2025-02-17 19:31:54 +0300

    first table

--- migrations/0006_create_society_table.sql
+++ migrations/0006_create_society_table.sql
@@ -24,3 +24,22 @@ CREATE TABLE IF NOT EXISTS society
 -- +goose StatementBegin
 DROP TABLE IF EXISTS society;
 -- +goose StatementEnd
+
+-- +goose StatementBegin
+
+CREATE TABLE IF NOT EXISTS societies
+(
+    id           SERIAL PRIMARY KEY,
+    name         VARCHAR(255)            NOT NULL,
+    description  TEXT                    NOT NULL,
+    is_private   BOOLEAN   DEFAULT FALSE NOT NULL,
+    direction_id INT                     NOT NULL,
+    owner_uuid   UUID                    NOT NULL,
+    photo_url    TEXT,
+    access_id    INT                     NOT NULL REFERENCES access_level (id),
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+-- +goose StatementEnd
+
