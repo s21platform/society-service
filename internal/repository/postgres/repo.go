929069fb514dcf6c504fc7c964f3a095bc439329
njohanne <aleksey88.avt@gmail.com>
2025-02-26 23:34:05 +0300

    squirrel

--- internal/repository/postgres/repo.go
+++ internal/repository/postgres/repo.go
@@ -5,25 +5,22 @@ import (
 	"log"
 	"time"
 
-	society "github.com/s21platform/society-proto/society-proto"
-
+	"github.com/Masterminds/squirrel"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
 	"github.com/lib/pq"
 
-	"github.com/google/uuid"
-
-	"github.com/s21platform/society-service/internal/model"
-
-	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq" // Импортируем данную библиотеку для работы с бд.
+	society "github.com/s21platform/society-proto/society-proto"
 	"github.com/s21platform/society-service/internal/config"
+	"github.com/s21platform/society-service/internal/model"
 )
 
 type Repository struct {
 	connection *sqlx.DB
+	sb         squirrel.StatementBuilderType
 }
 
 func connect(cfg *config.Config) (*Repository, error) {
-	// Connect db
 	conStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
 		cfg.Postgres.User, cfg.Postgres.Password, cfg.Postgres.Database, cfg.Postgres.Host, cfg.Postgres.Port)
 
@@ -32,12 +29,14 @@ func connect(cfg *config.Config) (*Repository, error) {
 		return nil, fmt.Errorf("sql.Open: %w", err)
 	}
 
-	return &Repository{db}, nil
+	return &Repository{
+		connection: db,
+		sb:         squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar),
+	}, nil
 }
 
 func New(cfg *config.Config) (*Repository, error) {
 	var err error
-
 	var repo *Repository
 
 	for i := 0; i < 5; i++ {
@@ -59,27 +58,39 @@ func (r *Repository) Close() {
 
 func (r *Repository) CreateSociety(socData *model.SocietyData) (string, error) {
 	var societyUUIDStr string
-	err := r.connection.QueryRowx("INSERT INTO society(name, description, owner_uuid, format_id, post_permission_id, is_search)"+
-		"VALUES ($1, $2, $3, $4, $5, $6) RETURNING id",
-		socData.Name,
-		"",
-		socData.OwnerUUID,
-		socData.FormatID,
-		socData.PostPermission,
-		socData.IsSearch).Scan(&societyUUIDStr)
+
+	query := r.sb.Insert("society").
+		Columns("name", "description", "owner_uuid", "format_id", "post_permission_id", "is_search").
+		Values(socData.Name, "", socData.OwnerUUID, socData.FormatID, socData.PostPermission, socData.IsSearch).
+		Suffix("RETURNING id")
+
+	sql, args, err := query.ToSql()
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to build SQL query: %v", err)
+	}
+
+	err = r.connection.QueryRowx(sql, args...).Scan(&societyUUIDStr)
+	if err != nil {
+		return "", fmt.Errorf("failed to insert society: %v", err)
 	}
 
 	societyUUID, err := uuid.Parse(societyUUIDStr)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to parse UUID: %v", err)
 	}
-	_, err = r.connection.Exec("INSERT INTO society_members(society_id, user_uuid, role, payment_status)"+
-		"VALUES ($1, $2, 1, 1)", societyUUID, socData.OwnerUUID)
 
+	query = r.sb.Insert("society_members").
+		Columns("society_id", "user_uuid", "role", "payment_status").
+		Values(societyUUID, socData.OwnerUUID, 1, 1)
+
+	sql, args, err = query.ToSql()
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to build SQL query: %v", err)
+	}
+
+	_, err = r.connection.Exec(sql, args...)
+	if err != nil {
+		return "", fmt.Errorf("failed to insert society member: %v", err)
 	}
 
 	return societyUUID.String(), nil
@@ -87,28 +98,32 @@ func (r *Repository) CreateSociety(socData *model.SocietyData) (string, error) {
 
 func (r *Repository) GetSocietyInfo(societyUUID string) (*model.SocietyInfo, error) {
 	var societyInfo model.SocietyInfo
-
-	query := `SELECT 
-		s.name, 
-		s.description, 
-		s.owner_uuid, 
-		s.photo_url, 
-		s.format_id, 
-		s.post_permission_id, 
-		s.is_search, 
-		COALESCE(COUNT(mr.user_uuid), 0) AS count_subscribe, 
-		ARRAY_REMOVE(ARRAY_AGG(sha.tag_id), NULL) AS tags_id
-	FROM society s
-	LEFT JOIN members_requests mr ON s.id = mr.society_id AND mr.status_id = 1 -- 1 означает, что пользователь принят в сообщество
-	LEFT JOIN society_has_tags sha ON s.id = sha.society_id AND sha.is_active = TRUE
-	WHERE s.id = $1
-	GROUP BY s.id;`
-
-	row := r.connection.QueryRow(query, societyUUID)
-
 	var tags pq.Int64Array
 
-	err := row.Scan(
+	query := r.sb.Select(
+		"s.name",
+		"s.description",
+		"s.owner_uuid",
+		"s.photo_url",
+		"s.format_id",
+		"s.post_permission_id",
+		"s.is_search",
+		"COALESCE(COUNT(mr.user_uuid), 0) AS count_subscribe",
+		"ARRAY_REMOVE(ARRAY_AGG(sha.tag_id), NULL) AS tags_id",
+	).
+		From("society s").
+		LeftJoin("members_requests mr ON s.id = mr.society_id AND mr.status_id = 1").
+		LeftJoin("society_has_tags sha ON s.id = sha.society_id AND sha.is_active = TRUE").
+		Where(squirrel.Eq{"s.id": societyUUID}).
+		GroupBy("s.id")
+
+	sql, args, err := query.ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build SQL query: %v", err)
+	}
+
+	row := r.connection.QueryRow(sql, args...)
+	err = row.Scan(
 		&societyInfo.Name,
 		&societyInfo.Description,
 		&societyInfo.OwnerUUID,
@@ -119,43 +134,57 @@ func (r *Repository) GetSocietyInfo(societyUUID string) (*model.SocietyInfo, err
 		&societyInfo.CountSubscribe,
 		&tags,
 	)
-
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to scan society info: %v", err)
 	}
 
 	societyInfo.TagsID = tags
-
 	return &societyInfo, nil
 }
 
 func (r *Repository) UpdateSociety(societyData *society.UpdateSocietyIn, peerUUID string) error {
 	if !isOwnerAdminModerator(peerUUID, societyData.SocietyUUID, r) {
-		return fmt.Errorf("faild to peer not Owner, Admin or Moderator to update society")
+		return fmt.Errorf("failed to peer not Owner, Admin or Moderator to update society")
 	}
-	_, err := r.connection.Exec("update society set name = $1, description = $2,"+
-		"photo_url = $3, format_id = $4, post_permission_id = $5, is_search = $6 where id = $7",
-		societyData.Name,
-		societyData.Description,
-		societyData.PhotoURL,
-		societyData.FormatID,
-		societyData.PostPermission,
-		societyData.IsSearch,
-		societyData.SocietyUUID)
+
+	query := r.sb.Update("society").
+		Set("name", societyData.Name).
+		Set("description", societyData.Description).
+		Set("photo_url", societyData.PhotoURL).
+		Set("format_id", societyData.FormatID).
+		Set("post_permission_id", societyData.PostPermission).
+		Set("is_search", societyData.IsSearch).
+		Where(squirrel.Eq{"id": societyData.SocietyUUID})
+
+	sql, args, err := query.ToSql()
 	if err != nil {
-		return fmt.Errorf("faild to update society: %w", err)
+		return fmt.Errorf("failed to build SQL query: %v", err)
 	}
+
+	_, err = r.connection.Exec(sql, args...)
+	if err != nil {
+		return fmt.Errorf("failed to update society: %v", err)
+	}
+
 	return nil
 }
 
 func isOwnerAdminModerator(peerUUID, societyUUID string, r *Repository) bool {
 	var role int
-	err := r.connection.QueryRowx("select role from society_members where society_id = $1 and user_uuid = $2", societyUUID, peerUUID).Scan(&role)
+
+	query := r.sb.Select("role").
+		From("society_members").
+		Where(squirrel.Eq{"society_id": societyUUID, "user_uuid": peerUUID})
+
+	sql, args, err := query.ToSql()
 	if err != nil {
 		return false
 	}
-	if role == 1 || role == 2 || role == 3 {
-		return true
+
+	err = r.connection.QueryRowx(sql, args...).Scan(&role)
+	if err != nil {
+		return false
 	}
-	return false
+
+	return role == 1 || role == 2 || role == 3
 }
